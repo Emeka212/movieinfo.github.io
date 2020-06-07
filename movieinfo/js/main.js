92e5eeef962e0b05b40f812a67c0882aafcc35db
Stephen Michael <60589175+Emeka212@users.noreply.github.com>
2020-06-07 03:45:41 +0100

    Add files via upload

--- movieinfo/js/main.js
+++ movieinfo/js/main.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
 });
 
 function getMovies(searchText){
-	axios.get('http://www.omdbapi.com?s='+searchText+'&r=json&apikey=a099c99a')
+	axios.get('https://www.omdbapi.com?s='+searchText+'&r=json&apikey=a099c99a')
 	.then((response) => {
 		console.log(response);
 		let movies = response.data.Search;
@@ -40,7 +40,7 @@ function movieSelected(id){
 function getMovie(){
 	let movieId = sessionStorage.getItem('movieId');
     
-    axios.get('http://www.omdbapi.com?i='+movieId+'&r=json&apikey=a099c99a')
+    axios.get('https://www.omdbapi.com?i='+movieId+'&r=json&apikey=a099c99a')
 	.then((response) => {
 		console.log(response);
 		let movie = response.data;
@@ -68,7 +68,7 @@ function getMovie(){
              <h3>Plot</h3>
              ${movie.Plot}
              <hr>
-             <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View In IMDB</a>
+             <a href="https://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View In IMDB</a>
              <a href="index2.html" class="btn btn-default">Go Back To Search</a>
              
             </div>
